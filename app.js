424e27c5079af130ae4ed92e612fc3471b1c2ede
stupiddog <shih820517@gmail.com>
2014-05-02 23:48:00 +0800

    add descriptions of photos and table format

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head></head><body>"
+  var data = "<html><head></head><body><table><tr><td>Photo</td><td>description</td></tr>"
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+      data += "<tr><td><img src='" + val.images[5].source + "'></td><td>" + val.name + "</td></tr>";
     });
     
-    data += "</body></html>";
+    data += "</table></body></html>";
     res.end(data);
   });
 
